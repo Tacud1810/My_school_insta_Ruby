eb11de9d6364f5f9537ca21680898850be97aabd
Tacud1810 <r.kachyna@gmail.com>
2023-09-06 21:23:08 +0200

    Changes to be committed: 	new file:   app/controllers/home_controller.rb 	modified:   app/controllers/posts_controller.rb 	new file:   app/helpers/home_helper.rb 	modified:   app/models/post.rb 	new file:   app/views/home/about.html.erb 	modified:   app/views/layouts/_navbar.html.erb 	modified:   app/views/posts/_form.html.erb 	modified:   app/views/posts/_post.html.erb 	modified:   config/routes.rb 	new file:   db/migrate/20230906180547_create_active_storage_tables.active_storage.rb 	modified:   db/schema.rb 	new file:   test/controllers/home_controller_test.rb
    přidána about page
    přidány obrázky

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
 	validates :title, presence: true, length: {minimum:5, maximum: 100}
 	validates :description, presence: true, length: {minimum:5, maximum: 500}
 	validates :keywords, presence: true, length: {minimum:5}
+
+	has_many_attached :images
+
+	def self.order_by_created_at_desc
+		order(created_at: :desc)
+	end
 end
