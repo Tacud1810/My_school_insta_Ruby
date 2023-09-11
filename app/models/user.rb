ccefb492a75b441ad0a3376dfa09ddb5f8ebc9c0
Tacud1810 <r.kachyna@gmail.com>
2023-09-11 21:43:12 +0200

    modified:   app/models/user.rb Added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-
+	validates :username, presence: true, length:{minimum: 3, maximum: 25}, uniqueness: {case_sensitive: false}
+	VALID_EMAIL_REGEX = /([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/
+	validates :email, presence: true, length:{maximum: 105}, uniqueness: {case_sensitive: false},
+										format: {with: VALID_EMAIL_REGEX}
 end
