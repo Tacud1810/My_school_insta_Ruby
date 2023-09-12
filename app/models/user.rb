8cc5fcb1e7eff83f2e820632c9d3d8068bc15261
Tacud1810 <r.kachyna@gmail.com>
2023-09-12 11:35:31 +0200

    bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+	before_save {email = self.email.downcase}
 	has_many :posts
 	
 	validates :username, presence: true, length:{minimum: 3, maximum: 25}, uniqueness: {case_sensitive: false}
