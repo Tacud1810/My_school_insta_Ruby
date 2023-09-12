0bb66412c4a7c74ef69ab86c9ee328866216ea9a
Tacud1810 <r.kachyna@gmail.com>
2023-09-12 10:56:55 +0200

    modified:   app/controllers/posts_controller.rb 	modified:   app/models/post.rb 	modified:   app/models/user.rb 	new file:   app/views/layouts/_messages.html.erb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/posts/_form.html.erb 	modified:   app/views/posts/index.html.erb 	new file:   db/migrate/20230912075144_add_user_id_to_posts.rb 	modified:   db/schema.rb
    added user_id to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
 
+	belongs_to :user
+
 	validates :title, presence: true, length: {minimum:5, maximum: 100}
 	validates :description, presence: true, length: {minimum:5, maximum: 500}
 	validates :keywords, presence: true, length: {minimum:5}
