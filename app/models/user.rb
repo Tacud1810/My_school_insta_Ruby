b00c38c7709d729ff2a537e4ab742ddd0521363b
Tacud1810 <r.kachyna@gmail.com>
2023-09-19 12:10:13 +0200

    Users Signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/
 	validates :email, presence: true, length:{maximum: 105}, uniqueness: {case_sensitive: false},
 										format: {with: VALID_EMAIL_REGEX}
+	has_secure_password	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
+  get 'signup', to:'users#new'
+  resources :users, except: [:new]
 end
